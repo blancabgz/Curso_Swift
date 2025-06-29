5cad9ee9613212564949b3502039d948999b7e58
blancabgz <blancabril.999@gmail.com>
2025-06-29 13:11:13 +0200

    Ejemplo de lista

---
+++ CursoIOS/Components/ListExample.swift
@@ -0,0 +1,18 @@
+//
+//  ListExample.swift
+//  CursoIOS
+//
+//  Created by Blanca Abril Gonzalez on 24/6/25.
+//
+
+import SwiftUI
+
+struct ListExample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListExample()
+}
